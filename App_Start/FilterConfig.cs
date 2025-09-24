8a0ba42b18c43483e1759bc9e16b7f358cc762f5
PJSA <PJSA@DESKTOP-P3KSKLA>
2025-09-23 20:42:19 -0500

    Agregar archivos de proyecto.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PRUEBA_UX_UI__NEOCONT_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRUEBA_UX_UI__NEOCONT_.MvcApplication" Language="C#" %>
