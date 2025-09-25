4eafc468de8bf2e34f2f83decd3a5ae62a2df89f
PJSA0127 <170378557+PJSA0127@users.noreply.github.com>
2025-09-25 01:35:54 -0500

    add: creacion del area Contabilidad

---
+++ Areas/Contabilidad/Controllers/TransaccionContableController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRUEBA_UX_UI__NEOCONT_.Areas.Contabilidad.Controllers
+{
+    public class TransaccionContableController : Controller
+    {
+
+        public ActionResult IngresoAsientosContablesManuales()
+        {
+            ViewBag.Message = "IngresoAsientosContablesManuales";
+            return View();
+        }
+
+        public ActionResult SolicitudIngresoAsientoOperativos()
+        {
+            ViewBag.Message = "SolicitudIngresoAsientoOperativos";
+            return View();
+        }
+
+        //public ActionResult IngresoAsientosContablesManuales()
+        //{
+        //    try
+        //    {
+        //        // Ejemplo de lógica
+        //        var datos = ObtenerDatos();
+        //        if (datos == null)
+        //        {
+        //            throw new Exception("No se encontraron datos para mostrar.");
+        //        }
+
+        //        return View(datos);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Registrar el error (ej: log a archivo o base de datos)
+        //        System.Diagnostics.Debug.WriteLine(ex);
+
+        //        // Mandar un mensaje controlado a la vista de Error
+        //        ViewBag.ErrorMessage = ex.Message;
+        //        return View("~/Views/Shared/Error.cshtml");
+        //    }
+        //}
+    }
+}
